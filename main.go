7b9d60d10a84d8a079256499f6d89f094cebace6
dwskme <officialsamirkc@gmail.com>
2024-03-19 17:25:47 +0545

    chore: improve input handling

--- main.go
+++ main.go
@@ -12,9 +12,6 @@ import (
 func main() {
 	var dumpPath, query string
 	flag.StringVar(&dumpPath, "p", "temp/enwiki-latest-abstract1.xml.gz", "wiki abstract dump path")
-	fmt.Print("Enter search query: ")
-	fmt.Scanln(&query)
-	flag.StringVar(&query, "q", query, "search query")
 	flag.Parse()
 
 	log.Println("Running Full Text Search")
@@ -26,17 +23,25 @@ func main() {
 	}
 	log.Printf("Loaded %d documents in %v", len(docs), time.Since(start))
 
-	start = time.Now()
 	idx := make(utils.Index)
 	idx.Add(docs)
-	log.Printf("Indexed %d documents in %v", len(docs), time.Since(start))
+	log.Printf("Indexed %d documents", len(docs))
 
-	start = time.Now()
-	matchedIDs := idx.Search(query)
-	log.Printf("Search found %d documents in %v", len(matchedIDs), time.Since(start))
+	for {
+		fmt.Print("\nEnter search query (type 'exit' to quit): ")
+		fmt.Scanln(&query)
+		if query == "exit" {
+			break
+		}
 
-	for _, id := range matchedIDs {
-		doc := docs[id]
-		log.Printf("%d\t%s\n", id, doc.Text)
+		start = time.Now()
+		matchedIDs := idx.Search(query)
+		log.Printf("Search found %d documents in %v", len(matchedIDs), time.Since(start))
+
+		for _, id := range matchedIDs {
+			doc := docs[id]
+			log.Printf("%d\t%s\n", id, doc.Text)
+		}
 	}
 }
+
